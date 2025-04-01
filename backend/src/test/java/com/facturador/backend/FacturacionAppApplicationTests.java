65c13252933333ccb58ba84b6f91e0b5b5e4f47b
Seba-Archlinux <avila.sebastianm@gmail.com>
2025-04-01 15:22:05 -0300

    85625

---
+++ backend/src/test/java/com/facturador/backend/FacturacionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturador.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
